52742f319c5a55aff5cc152c1f72e21df4fc8c54
Nicolas <nico22utn@gmail.com>
2017-11-23 18:49:11 -0300

    Mapeo Completo Nicolas Con Entidad Consulta

--- src/Modelo/Consulta.java
+++ src/Modelo/Consulta.java
@@ -5,10 +5,74 @@
  */
 package Modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author User
  */
-public class Consulta {
+@Entity
+@Table(name="consulta")
+public class Consulta implements Serializable {
+    @Id @GeneratedValue
+    private Long id;
+    @Column
+    private Long numeroConsulta;
+    @ManyToOne
+    private TipoConsulta tipoConsulta;
+    @ManyToOne
+    private EstadoConsulta estadoConsulta;
+    @ManyToOne
+    private Paciente paciente;
+
+    public Consulta() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public EstadoConsulta getEstadoConsulta() {
+        return estadoConsulta;
+    }
+
+    public void setEstadoConsulta(EstadoConsulta estadoConsulta) {
+        this.estadoConsulta = estadoConsulta;
+    }
+
+    public Long getNumeroConsulta() {
+        return numeroConsulta;
+    }
+
+    public void setNumeroConsulta(Long numeroConsulta) {
+        this.numeroConsulta = numeroConsulta;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public TipoConsulta getTipoConsulta() {
+        return tipoConsulta;
+    }
+
+    public void setTipoConsulta(TipoConsulta tipoConsulta) {
+        this.tipoConsulta = tipoConsulta;
+    }
+    
     
 }
